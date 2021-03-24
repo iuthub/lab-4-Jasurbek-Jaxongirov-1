414d1c105bb9aaae0aa167d207291b7a9731c7c3
Jasur Jakhongirov <jahongirovjasurbek@gmail.com>
2021-03-24 16:47:18 +0500

    changes applied

---
+++ webpage/music.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Music Viewer</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="viewer.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+<div id="header">
+
+    <h1>190M Music Playlist Viewer</h1>
+    <h2>Search Through Your Playlists and Music</h2>
+</div>
+
+
+<div id="listarea">
+    <ul id="musiclist">
+        <?php
+            $root = "./";
+            $song_folder = $root."/songs";
+            $music_files = glob($song_folder."/*.m*");
+            foreach($music_files as $file_names){
+        ?>
+        <li class="mp3item">
+            <a href="songs/<?= $file_names ?>>"><?= basename($file_names) ?></a>
+        </li>
+        <?php
+            }
+        ?>
+        <?php
+        $root = "./";
+        $song_folder = $root."/songs";
+        $text_files = glob($song_folder."/*.txt");
+        foreach($text_files as $file_names){
+            ?>
+            <li class="playlistitem">
+                <a href="songs/<?= $file_names ?>>"><?= basename($file_names) ?>></a>
+            </li>
+            <?php
+        }
+        ?>
+
+    </ul>
+</div>
+</body>
+</html>
